da8cb7a0fd09354049e5cfe0f68a9ee4d05a5f66
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2023-12-04 18:30:31 -0500

    Add post thumbnail and link to post in footer.php

--- themes/twentytwentyone-child/footer.php
+++ themes/twentytwentyone-child/footer.php
@@ -39,9 +39,11 @@
 				$post_query -> the_post();
 				?>
 				<div class="post">
-					<h2><?php the_title(); ?></h2>
+					<?php 
+					the_post_thumbnail('thumbnail');
+					?>
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 					<p><?php the_excerpt(); ?></p>
-
 					<br>
 				</div>
 				<?php
