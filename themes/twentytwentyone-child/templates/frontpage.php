f62d2ac5611830f1e5d4544f7af7ff3d14139ab9
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2023-12-04 13:02:14 -0500

    creating Frontpage template

---
+++ themes/twentytwentyone-child/templates/frontpage.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * Template Name: Frontpage
+ */
+
+
+ get_header();
+
+ echo '<h1>Frontpage template</h1>';
+
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+endwhile; // End of the loop.
+
+get_footer();
