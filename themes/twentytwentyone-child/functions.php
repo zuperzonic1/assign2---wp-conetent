7a8ece834c550ff13706fe55f98958178dd7b02f
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2023-12-04 18:31:02 -0500

    Add background color

--- themes/twentytwentyone-child/functions.php
+++ themes/twentytwentyone-child/functions.php
@@ -9,20 +9,14 @@
  * @since Twenty Twenty-One 1.0
  */
 
-    add_action( 'wp_enqueue_scripts', 'twentytwentyonechild_wp_enqueue_scripts' );
-    function twentytwentyonechild_wp_enqueue_scripts() {
-        wp_enqueue_style( 'twentytwentyonechild-style', 
-        get_stylesheet_uri(), 
-        array( 'twenty-twenty-one-style' ), 
-        wp_get_theme()->get('Version') 
-        );
-        
-        wp_enqueue_style( 'index-style', 
-        get_stylesheet_uri() . 'assets/css/index.css',
-        array(  ), 
-        wp_get_theme()->get('1.0') 
-        );
-        
-    }
-    define( 'DISALLOW_FILE_EDIT', true );
+add_action( 'wp_enqueue_scripts', 'twentytwentyonechild_wp_enqueue_scripts' );
+function twentytwentyonechild_wp_enqueue_scripts() {
+    wp_enqueue_style( 'index', 
+    get_stylesheet_directory_uri() . '/assets/css/index.css',
+    array(), 
+    wp_get_theme()->get('Version') 
+    );
+}
+
+define( 'DISALLOW_FILE_EDIT', true );
 ?>
